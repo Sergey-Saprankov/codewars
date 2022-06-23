1fc1bbc818c6193c76bca72497ed39e572fda4b2
Sergey Saprankov <sergey-saprankov@mail.ru>
2022-06-23 12:54:45 +0300

    add new kata

---
+++ 7kyu/3.js
@@ -0,0 +1,7 @@
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+// Examples (Input -> Output)
+
+const min = list => Math.min.apply(null, list);
+
+const max = list => Math.max.apply(null, list);
